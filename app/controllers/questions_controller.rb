db0a330f59ee085e64f8dd2a14c32e893ee7ae0a
Neal Karpe <nealkarpe@gmail.com>
2017-10-23 22:13:31 +0530

    initial commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,51 @@
+class QuestionsController < ApplicationController
+	
+	def new
+  		@question = Question.new()
+	end
+
+	def index
+		@questions = Question.all
+	end
+
+	def show
+		@question = Question.find(params[:id])
+	end
+
+	def create
+		#render plain: params[:post].inspect
+		@question = Question.new(question_params)
+		if(@question.save)
+			redirect_to @question
+		else
+			render "new"
+		end
+	end
+
+
+	def edit
+		@question = Question.find(params[:id])
+	end
+
+	def update
+		@question = Question.find(params[:id])
+		if(@question.update(question_params))
+			redirect_to @question
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@question = Question.find(params[:id])
+		@question.destroy
+
+		redirect_to questions_path
+	end
+
+
+	private def question_params
+		params.require(:question).permit(:qtext, :description)
+	end
+
+end
